56745d7f9e064a93f5fc9ca37668d458545aac3e
rudra singh <rudrathik433@gmail.com>
2026-06-24 14:04:27 +0530

    Add files via upload
    day 5 of learning new topic list in python

---
+++ my_list_.py
@@ -0,0 +1,9 @@
+# making a list of my favorite things and using .append .remove and using len() to know the lenght of the list
+my_favorite_things = ["biryani","god of war","alexandra daddario"]
+print (my_favorite_things)
+print (my_favorite_things[0])
+print (my_favorite_things[-1])
+my_favorite_things.append("chocolate")
+my_favorite_things.remove("biryani")
+print (my_favorite_things)
+print (len(my_favorite_things))
