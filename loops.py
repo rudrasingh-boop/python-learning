f7879088e9199aa2fccace92b545000c334ac137
rudra singh <rudrathik433@gmail.com>
2026-06-26 12:18:21 +0530

    Add files via upload
    day 6 loops in python

---
+++ loops.py
@@ -0,0 +1,17 @@
+# using while loop and for loop to this program
+my_favorite_things = ["reading books", "playing cricket", "alexandra daddario", "listening to music", "watching movies"]
+
+# Using a for loop
+for thing in my_favorite_things:
+    print(f"My favorite thing is {thing}.")
+
+# Using a for loop to print the range  of numbers from 1 to 10
+for i in range (1 , 11):
+    print(i)
+
+# using a while loop to print numbers from 5 to 1
+count = 5
+while count > 0:
+    print(count)
+    count = count-1    
+ 
